c284c3bd79b6e5399f240f5e88a50e1a1ceef266
Kalin <klyanevv@gmail.com>
2024-10-29 18:54:59 +0200

    Changed name

---
+++ movingZeroesToTheEnd.py
@@ -0,0 +1,10 @@
+# Moving Zeros to the End
+def exercise(input):
+    zerosToEnd=[]
+    for i in input:
+        if i == 0:
+            input.remove(i)
+            input.append(i)
+            
+    print(input)
+exercise([0, 1, 0, 3, 12])
